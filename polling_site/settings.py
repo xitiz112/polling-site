3ced02e5ffb8cb12736179be43949d073eff3dda
kshitiz <kshitizbasnet86@gmail.com>
2020-05-26 17:34:00 +0545

    updated static

--- polling_site/settings.py
+++ polling_site/settings.py
@@ -125,3 +125,8 @@ STATIC_ROOT= os.path.join(BASE_DIR,'_staticfiles_')
   
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
